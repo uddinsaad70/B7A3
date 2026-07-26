0433cf6a5c2f561be3801b265f9420ff896a6050
baagdaas <saad.cse.kuet@gmail.com>
2026-07-26 23:35:41 +0600

    Query 6: Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings

--- QUERY.sql
+++ QUERY.sql
@@ -111,3 +111,11 @@ SELECT
     b.booking_id
 FROM Users u
 LEFT JOIN Bookings b ON u.user_id = b.user_id;
+
+-- Query 6: Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings
+SELECT
+    booking_id,
+    match_id,
+    total_cost
+FROM Bookings
+WHERE total_cost > (SELECT AVG(total_cost) FROM Bookings);
