0bec38bba61ab7bb8bcb8dd9239929a479b96168
baagdaas <saad.cse.kuet@gmail.com>
2026-07-26 23:32:44 +0600

    Query 4: Retrieve match booking details along with the User's full name and the scheduled Match fixture teams

--- QUERY.sql
+++ QUERY.sql
@@ -93,3 +93,13 @@ SELECT
     COALESCE(payment_status, 'Action Required') AS systematic_status
 FROM Bookings
 WHERE payment_status IS NULL;   
+
+-- Query 4: Retrieve match booking details along with the User's full name and the scheduled Match fixture teams
+SELECT
+    b.booking_id,
+    u.full_name,
+    m.fixture,
+    b.total_cost
+FROM Bookings b
+INNER JOIN Users u ON b.user_id = u.user_id
+INNER JOIN Matches m ON b.match_id = m.match_id;
