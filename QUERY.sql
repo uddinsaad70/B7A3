ea54a7eece4610bed874cf359473adadf9c13c7a
baagdaas <saad.cse.kuet@gmail.com>
2026-07-26 23:33:54 +0600

    Query 5: Display a comprehensive list of all users and their booking IDs, ensuring that fans who have never bought a ticket are still listed

--- QUERY.sql
+++ QUERY.sql
@@ -103,3 +103,11 @@ SELECT
 FROM Bookings b
 INNER JOIN Users u ON b.user_id = u.user_id
 INNER JOIN Matches m ON b.match_id = m.match_id;
+
+-- Query 5: Display a comprehensive list of all users and their booking IDs, ensuring that fans who have never bought a ticket are still listed
+SELECT
+    u.user_id,
+    u.full_name,
+    b.booking_id
+FROM Users u
+LEFT JOIN Bookings b ON u.user_id = b.user_id;
