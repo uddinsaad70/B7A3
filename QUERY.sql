44a4dc1754476bba405d8f2029743ba8e65a2a35
baagdaas <saad.cse.kuet@gmail.com>
2026-07-26 23:21:35 +0600

    Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive)

--- QUERY.sql
+++ QUERY.sql
@@ -78,3 +78,9 @@ SELECT match_id, fixture, base_ticket_price
 FROM Matches
 WHERE tournament_category = 'Champions League'
   AND match_status = 'Available';
+
+-- Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive)
+SELECT user_id, full_name, email
+FROM Users
+WHERE full_name ILIKE 'Tanvir%'
+   OR full_name ILIKE '%Haque%';
